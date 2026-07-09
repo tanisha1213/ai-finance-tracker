ccb69f0d2802db13af55c712e1f045f4c823fcf4
tanisha1213 <tanishaagandhi2005@gmail.com>
2026-07-09 14:05:17 +0530

    fix: allow https://localhost in CORS allowedOrigins for Android WebView secure contexts

--- server/app.js
+++ server/app.js
@@ -19,6 +19,7 @@ const allowedOrigins = [
   process.env.FRONTEND_URL || 'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost',
+  'https://localhost',
   'capacitor://localhost'
 ];
 
